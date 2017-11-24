51a955317e22e0f66c08ee8b7bccacc4cfef6d07
Connie Kim <minjungck@gmail.com>
2017-11-24 01:48:18 -0800

    Initial commit

---
+++ src/Inode.java
@@ -0,0 +1,101 @@
+/*
+** Authors: Bobby Damore, Connie Kim
+** CSS 430
+** Inode.java
+*/
+
+/*
+** This class is a data structure used to represent a file or directory.
+** 
+*/
+public class Inode {
+   private final static int iNodeSize = 32;       // fix to 32 bytes
+   private final static int directSize = 11;      // # direct pointers
+   private final static int totalInodes = 16;
+
+   public int length;                             // file size in bytes
+   public short count;                            // # file-table entries pointing to this
+   public short flag;                             // 0 = unused, 1 = used, ...
+   public short direct[] = new short[directSize]; // direct pointers
+   public short indirect;                         // a indirect pointer
+
+   /* 
+   ** Default Constructor
+   */
+   Inode( ) {                                     // a default constructor
+      length = 0;
+      count = 0;
+      flag = 1;
+      for ( int i = 0; i < directSize; i++ )
+         direct[i] = -1;
+      indirect = -1;
+   }
+
+   /* 
+   ** Constructor
+   */
+   Inode( short iNumber ) {                       // retrieving inode from disk
+      // design it by yourself.
+      if(iNumber >= 0) {
+         int blockNum, offset;
+         byte [] data = new byte[Disk.blockSize];
+
+         blockNum = (iNumber / totalInodes) + 1;
+
+         SysLib.rawread(blockNum, data);
+
+         offset = (iNumber % totalInodes) * iNodeSize;
+
+         length = SysLib.bytes2int(data, offset);
+         offset += 4;
+         count = SysLib.bytes2short(data, offset);
+         offset += 2;
+         flag = SysLib.bytes2short(data, offset);
+         offset += 2;
+
+         for (int i = 0; i < directSize; i++, offset += 2)
+            direct[i] = SysLib.bytes2short(data, offset);
+
+         indirect = SysLib.bytes2short(data, offset);
+         offset += 2;
+      }
+      return;
+
+   }
+
+   /* 
+   ** toDisk method takes in an iNumber and 
+   ** writes the information of the corresponding 
+   ** Inode back into the disk.
+   */
+   int toDisk( short iNumber ) {                  // save to disk as the i-th inode
+      // design it by yourself.
+
+      if(iNumber >= 0) {
+         int blockNum, offset;
+         byte [] data = new byte[Disk.blockSize];
+
+         blockNum = (iNumber / totalInodes) + 1;
+         SysLib.rawread(blockNum, data);
+
+         offset = (iNumber % totalInodes) * iNodeSize;
+
+         SysLib.int2bytes(length, data, offset);
+         offset += 4;
+         SysLib.short2bytes(count, data, offset);
+         offset += 2;
+         SysLib.short2bytes(flag, data, offset);
+         offset += 2;
+
+         for (int i = 0; i < directSize; i++, offset += 2)
+            SysLib.short2bytes(direct[i], data, offset);
+
+         SysLib.short2bytes(indirect, data, offset);
+         offset += 2;
+         
+         SysLib.rawwrite(blockNum, data);
+      }
+      return;
+   }
+
+}
---
+++ src/Superblock.java
@@ -0,0 +1,72 @@
+/*
+** Authors: Bobby Damore, Connie Kim
+** CSS 430
+** Superblock.java
+*/
+
+class Superblock {
+   public int totalBlocks; // the number of disk blocks
+   public int totalInodes; // the number of inodes
+   public int freeList;    // the block number of the free list's head
+   
+   /* 
+   ** Constructor
+   */
+   public SuperBlock( int diskSize ) {
+		byte[] superblock = new byte[Disk.blockSize];
+
+		SysLib.rawread(0, superblock);
+		totalBlocks = SysLib.bytes2int(superblock, 0);
+		totalInodes = SysLib.bytes2int(superblock, 4);
+		freeList = SysLib.bytes2int(superblock, 8);
+
+		if (totalBlocks == diskSize && totalInodes > 0 && freeList >= 2)
+			return;
+
+		totalBlocks = diskSize;
+		format(64);
+	}
+
+	/* 
+	** Format method allows data to be cleared and restructures to the original format. 
+	** Superblock's data members will be cleared and will be updated accordingly by calling
+	** the synch() method.
+	*/
+	public void format(int inodes) {
+		totalInodes = inodes;
+
+		for(int i = 0; i < inodes; i++) {
+			Inode ins = new Inode();
+			ins.toDisk((short) i);
+		}
+
+		freeList = (totalInodes / 16) + 2;
+
+		for(int i = freeList; i < 64 - 1; i++) {
+			byte [] block = new byte[Disk.blockSize];
+
+			for(int j = 0; j < Disk.blockSize; j++)
+				block[j] = 0;
+
+			SysLib.int2bytes(i + 1, block, 0);
+			SysLib.rawwrite(i, block);
+		}
+
+		SysLib.int2bytes(-1, block, 0);
+		SysLib.rawwrite(defaultBlocks - 1, block);
+
+		synch();
+	}
+
+	/* 
+	** Synch will be used by format method to update
+	** information of the Superblock back to Disk.
+	*/
+	public void synch () {
+		byte [] superblock = new byte[Disk.blockSize];
+		SysLib.int2bytes(totalBlocks, block, 0);
+		SysLib.int2bytes(totalInodes, block, 4);
+		SysLib.int2bytes(freeList, block, 8);
+		SysLib.rawwrite(0, block);
+	}
+}
