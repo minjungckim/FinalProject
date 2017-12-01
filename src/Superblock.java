a068ce3303f98a8a3d7550b0231b67ad7456d308
Bobby Damore <damorer@uw.edu>
2017-12-01 15:05:19 -0800

    Finished Inode and Superblock
    Got rid of some magic numbers, and added comments. Fixed a few block
    numbers that were off by 1.

--- src/Superblock.java
+++ src/Superblock.java
@@ -5,6 +5,8 @@
 */
 
 class Superblock {
+   private final static int defaultTotalInodes = 64;
+
    public int totalBlocks; // the number of disk blocks
    public int totalInodes; // the number of inodes
    public int freeList;    // the block number of the free list's head
@@ -20,11 +22,13 @@ class Superblock {
 		totalInodes = SysLib.bytes2int(superblock, 4);
 		freeList = SysLib.bytes2int(superblock, 8);
 
+		// check that superblock contains expected data
 		if (totalBlocks == diskSize && totalInodes > 0 && freeList >= 2)
 			return;
 
+		// otherwise, format file system
 		totalBlocks = diskSize;
-		format(64);
+		format(defaultTotalInodes);
 	}
 
 	/* 
@@ -33,28 +37,29 @@ class Superblock {
 	** the synch() method.
 	*/
 	public void format(int inodes) {
+		
+		// initialize inodes number of new Inodes
 		totalInodes = inodes;
-
-		for(int i = 0; i < inodes; ++i) {
+		for(short i = 0; i < totalInodes; ++i) {
 			Inode ins = new Inode();
-			ins.toDisk((short) i);
+			ins.toDisk(i);
 		}
 
-		freeList = (totalInodes / 16) + 2;
-
-		for(int i = freeList; i < 64 - 1; ++i) {
-			byte [] block = new byte[Disk.blockSize];
-
-			for(int j = 0; j < Disk.blockSize; ++j)
-				block[j] = 0;
-
+		// freeList head points to first block after Inodes
+		freeList = ((totalInodes - 1) / (Disk.blockSize / Inode.iNodeSize)) + 2;
+		
+		// write pointer to next free block for each free block
+		byte [] block = new byte[Disk.blockSize];
+		for(int i = freeList; i < Disk.diskSize; ++i) {
 			SysLib.int2bytes(i + 1, block, 0);
 			SysLib.rawwrite(i, block);
 		}
 
+		// write -1 to last block to show end of freeList
 		SysLib.int2bytes(-1, block, 0);
-		SysLib.rawwrite(defaultBlocks - 1, block);
+		SysLib.rawwrite(Disk.diskSize - 1, block);
 
+		// synch superblock data back to disk
 		synch();
 	}
 
@@ -63,10 +68,11 @@ class Superblock {
 	** information of the Superblock back to Disk.
 	*/
 	public void synch () {
+		// write superblock contents to buffer, then to disk
 		byte [] superblock = new byte[Disk.blockSize];
-		SysLib.int2bytes(totalBlocks, block, 0);
-		SysLib.int2bytes(totalInodes, block, 4);
-		SysLib.int2bytes(freeList, block, 8);
-		SysLib.rawwrite(0, block);
+		SysLib.int2bytes(totalBlocks, superblock, 0);
+		SysLib.int2bytes(totalInodes, superblock, 4);
+		SysLib.int2bytes(freeList, superblock, 8);
+		SysLib.rawwrite(0, superblock);
 	}
 }
